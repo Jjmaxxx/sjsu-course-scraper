d50b636e60508258aba959ea03fa998444efe167
Justin <justlee888@gmail.com>
2024-02-19 13:56:50 -0800

    got dept names

--- index.py
+++ index.py
@@ -15,7 +15,9 @@ def initialize_driver():
     return webdriver.Chrome(service=service, options=options)
 
 def parseBody(body):
-    name = body.pop(0)
+    # name = body.pop(0)
+    deparment, course, name = body.pop(0).strip().split(" ",2)
+    name = name.split("- ", 1)[1]   
     units = body.pop(0)
     description = next((text for text in body if not text.startswith("Prerequisite(s)") and not text.startswith("Grading: ")), None)
     prereqs = next((text for text in body if text.startswith("Prerequisite(s)") or text.startswith("Pre/Corequisite(s)")), None)
@@ -26,28 +28,28 @@ def parseBody(body):
     sustainability = next((text for text in body if text.startswith("Sustainability")), None)
     notes = next((text for text in body if "Note(s):" in text), None)
     
-    return [name,units,description,prereqs,satisfies,grading,repeatable,crosslist,sustainability,notes]
+    return [deparment, course, name,units,description,prereqs,satisfies,grading,repeatable,crosslist,sustainability,notes]
 
 #need this bc the course website sometimes just doesnt load correctly for some reason and needs a refresh
 #why is sj's own course website broken
-def findRows(driver):
+def findDeptName(driver):
     #try 10 reloads
     for i in range(10):
         try:
-            rows = WebDriverWait(driver,5).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME,"width"))
-            )
-            return rows
+            deptName = WebDriverWait(driver,5).until(
+                EC.presence_of_element_located((By.TAG_NAME,"strong"))
+            ).text
+            return deptName
         except:
             driver.refresh()
             continue
-    return None
+    print("found nothing")
+    exit()
 
 def getClassesData(driver,courses):
-    rows = findRows(driver)
-    if rows is None:
-        print('didnt find anything')
-        return;    
+    rows = WebDriverWait(driver,5).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME,"width"))
+    )
     for row in rows:
         link = WebDriverWait(row,5).until(
             EC.presence_of_element_located((By.TAG_NAME,"a"))
@@ -63,28 +65,33 @@ def getClassesData(driver,courses):
 
 def scrape_courses(prefixes):
     courses= []
+    departmentNames = []
     driver = initialize_driver()
     for tag in prefixes:
         driver.get(f'https://catalog.sjsu.edu/content.php?filter[27]={tag}&filter[29]=&filter[keyword]=&filter[32]=1&filter[cpage]=1&cur_cat_oid=14&expand=&navoid=5106&search_database=Filter&filter[exact_match]=1#acalog_template_course_filter')
         print(tag)
+        deptName = findDeptName(driver)     
+        departmentNames.append([tag,deptName])
         getClassesData(driver,courses)
         try:
         #sometimes the page has a page 2...
             driver.find_element(By.CSS_SELECTOR, '[aria-label="Page 2"]').click()
+            findDeptName(driver)
             getClassesData(driver,courses)
         except:
             continue
-    return courses
+    return [courses, departmentNames]
 
-def save_to_csv(data):
-    filename='courses.csv'
+def save_to_csv(filename, data):
     with open(filename, 'w', encoding="utf-8", newline='') as file:
         writer = csv.writer(file)
+        if filename == "courses.csv":
+            writer.writerow(["Department","Course","Name","Units","Description","Prereqs","Satisfies","Grading","Repeatable","Crosslist","Sustainability","Notes"])
         writer.writerows(data)
 
-courses= scrape_courses(prefix)
-save_to_csv(courses)
-
+courses,departments = scrape_courses(prefix)
+save_to_csv('courses.csv',courses)
+save_to_csv('departments.csv',departments)
 
 
 
